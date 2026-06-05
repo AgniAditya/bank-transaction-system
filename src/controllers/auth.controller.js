feb089d5bbdd9acda67f3b120e2b3a7c8cbedcf0
AgniAditya <agniaditya05@gmail.com>
2026-06-06 02:30:00 +0530

    feat: add user login functionality and fix password hashing

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,9 +3,7 @@ const jwt = require("jsonwebtoken");
 
 /**
  * register new user
- * POST request /api/auth/register
- * 1. get email, name, password from request body
- * 2. create a new user
+ * POST /api/auth/register
  */
 async function registerUser(req, res) {
   try {
@@ -35,7 +33,7 @@ async function registerUser(req, res) {
       },
     );
 
-    res.cookies("token", token);
+    res.cookie("token", token);
 
     return res.status(201).json({
       user: {
@@ -52,6 +50,56 @@ async function registerUser(req, res) {
   }
 }
 
+/*
+ * login user
+ * GET /api/auth/login
+ */
+async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(401).json({
+        message: "email not found",
+      });
+    }
+
+    const isValidePassword = await user.comparePassword(password);
+    if (!isValidePassword) {
+      return res.status(401).json({
+        message: "password is incorrect",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "3d",
+      },
+    );
+
+    res.cookie("token", token);
+
+    return res.status(200).json({
+      user: {
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+      },
+      message: "user login successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+}
+
 module.exports = {
   registerUser,
+  loginUser,
 };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -31,18 +31,18 @@ const userSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-userSchema.pre("save", async (next) => {
-  if (!this.isModified("passwrod")) {
-    return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
   }
 
   const hash = await bcrypt.hash(this.password, 10);
   this.password = hash;
 
-  return next();
+  return;
 });
 
-userSchema.methods.comparePassword = async (password) => {
+userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
